ea9d5e654d4bdf8fd1ff67d8cd6068afa4cb53a5
ruan-narici <ruannarici@hotmail.com>
2024-08-12 21:02:44 -0300

    feat: ADD findFirst with stream()

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -36,7 +36,7 @@ public class Principal {
 //        listaTemporadas.forEach(System.out::println);
 
         // Imprimindo o titulo dos episodios utilizando lambda
-        listaTemporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+//        listaTemporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
 
         // Criando uma unica lista contendo todos os episodios
         List<DadosEpisodio> listaEpisodios = listaTemporadas.stream()
@@ -44,32 +44,57 @@ public class Principal {
                 .collect(Collectors.toList());
 
         // Top 5 Episodios:
-        System.out.println("\n TOP 5 Episodios");
-        listaEpisodios.stream()
-                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
-                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
-                .limit(5)
-                .forEach(System.out::println);
+//        System.out.println("\n TOP 5 Episodios");
+//        listaEpisodios.stream()
+//                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+//                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+//                .limit(5)
+//                .forEach(System.out::println);
 
         // Criando novos objetos do tipo Episodio apos iterar a lista de temporadas
-        List<Episodio> listaObjetosEpisodios = listaTemporadas.stream()
-                .flatMap(t -> t.episodios().stream()
-                        .map(e -> new Episodio(t.numero(), e)))
-                .collect(Collectors.toList());
+//        List<Episodio> listaObjetosEpisodios = listaTemporadas.stream()
+//                .flatMap(t -> t.episodios().stream()
+//                        .map(e -> new Episodio(t.numero(), e)))
+//                .collect(Collectors.toList());
 //        System.out.println(listaObjetosEpisodios);
 
-        System.out.println("Digite o ano buscado: ");
-        Integer anoBuscado = leitor.nextInt();
-        leitor.nextLine();
-        LocalDate dataBuscada = LocalDate.of(anoBuscado, 1, 1);
-        DateTimeFormatter formatadorData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//        System.out.println("Digite o ano buscado: ");
+//        Integer anoBuscado = leitor.nextInt();
+//        leitor.nextLine();
+//        LocalDate dataBuscada = LocalDate.of(anoBuscado, 1, 1);
+//        DateTimeFormatter formatadorData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//
+//        listaObjetosEpisodios.stream()
+//                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBuscada))
+//                .forEach(e -> System.out.println(
+//                        "Temporada: " + e.getNumeroTemporada() +
+//                                " Titulo: " + e.getTitulo() +
+//                                " Data lançamento: " + e.getDataLancamento().format(formatadorData)
+//                ));
 
-        listaObjetosEpisodios.stream()
-                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBuscada))
-                .forEach(e -> System.out.println(
-                        "Temporada: " + e.getNumeroTemporada() +
-                                " Titulo: " + e.getTitulo() +
-                                " Data lançamento: " + e.getDataLancamento().format(formatadorData)
-                ));
+        // Listando o top 10 episodios
+        List<Episodio> listaTop10Episodios = listaTemporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                        .map(e -> new Episodio(t.numero(), e)))
+                .sorted(Comparator.comparing(Episodio::getAvaliacao).reversed())
+                .limit(10)
+                .collect(Collectors.toList());
+
+        listaTop10Episodios.stream()
+                .forEach(e -> System.out.println(e.getTitulo()));
+
+        // Buscando um episodio pelo titulo
+        System.out.println("Digite o titulo do episódio: ");
+        String episodioBuscado = leitor.nextLine();
+
+        Optional<Episodio> episodioEncontrado = listaTop10Episodios.stream()
+                .filter(e -> e.getTitulo().toUpperCase().contains(episodioBuscado.toUpperCase()))
+                .findFirst();
+
+        System.out.println(episodioEncontrado);
+
+
+
     }
 }
