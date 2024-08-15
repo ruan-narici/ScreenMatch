8d1a9325781eff6ef36a9f2a04337ecc9bfcd63d
ruan-narici <ruannarici@hotmail.com>
2024-08-15 19:19:27 -0300

    feat: ADD new attribute for DadosSerie record

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -8,7 +8,11 @@ public record DadosSerie(
         @JsonAlias("Title") String titulo,
         @JsonAlias("totalSeasons") Integer temporadas,
         @JsonAlias("Year") String ano,
-        @JsonAlias("imdbRating") String avaliacao
+        @JsonAlias("imdbRating") String avaliacao,
+        @JsonAlias("Genre") String genero,
+        @JsonAlias("Actors") String atores,
+        @JsonAlias("Poster") String baner,
+        @JsonAlias("Plot") String sinopse
 ) {
 
 }
