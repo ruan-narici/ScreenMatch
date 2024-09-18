8296016a7437eb535bbde413a6b0aa9013aeeca8
ruan-narici <ruannarici@hotmail.com>
2024-09-17 22:56:08 -0300

    feat: Create new SerieDTO

---
+++ src/main/java/br/com/alura/screenmatch/dto/SerieDTO.java
@@ -0,0 +1,22 @@
+package br.com.alura.screenmatch.dto;
+
+import br.com.alura.screenmatch.model.Categoria;
+import br.com.alura.screenmatch.model.Episodio;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public record SerieDTO(
+        Long id,
+        String titulo,
+        Integer temporada,
+        String ano,
+        Double avaliacao,
+        Categoria genero,
+        String atores,
+        String baner,
+        String sinopse,
+        List<Episodio> episodios
+) {
+}
