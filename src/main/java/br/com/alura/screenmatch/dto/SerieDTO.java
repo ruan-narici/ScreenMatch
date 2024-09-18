1422e22557b6657e2b824cf384ce45769df12068
ruan-narici <ruannarici@hotmail.com>
2024-09-17 23:36:43 -0300

    feat: ADD devtools

--- src/main/java/br/com/alura/screenmatch/dto/SerieDTO.java
+++ src/main/java/br/com/alura/screenmatch/dto/SerieDTO.java
@@ -1,11 +1,6 @@
 package br.com.alura.screenmatch.dto;
 
 import br.com.alura.screenmatch.model.Categoria;
-import br.com.alura.screenmatch.model.Episodio;
-import jakarta.persistence.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public record SerieDTO(
         Long id,
