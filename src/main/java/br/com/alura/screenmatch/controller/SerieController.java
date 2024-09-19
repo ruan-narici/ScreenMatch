ff9f2326bfe228912e1ec12b91bdb656d5df22fb
ruan-narici <ruannarici@hotmail.com>
2024-09-19 19:59:29 -0300

    feat: ADD new service *SerieService

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,34 +1,21 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
-import br.com.alura.screenmatch.repository.SerieRepository;
+import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
     @Autowired
-    SerieRepository serieRepository;
+    SerieService serieService;
 
     @GetMapping("/series")
     public List<SerieDTO> obterSeries() {
-        return serieRepository.findAll().stream()
-                .map(s -> new SerieDTO(
-                        s.getId(),
-                        s.getTitulo(),
-                        s.getTemporada(),
-                        s.getAno(),
-                        s.getAvaliacao(),
-                        s.getGenero(),
-                        s.getAtores(),
-                        s.getBaner(),
-                        s.getSinopse()
-                ))
-                .collect(Collectors.toList());
+        return serieService.obterTodasSeries();
     }
 }
