4e97f643f46cc311114e93fe97e1796d5da84958
ruan-narici <ruannarici@hotmail.com>
2024-11-23 20:46:43 -0300

    doc: ADD README

---
+++ README.md
@@ -0,0 +1,16 @@
+## Java & Springboot
+
+```
+O Sistema consome a API do site OMDB e lista as séries e filmes.
+```
+
+[![Alura](./assets/img/alura_img.jpg)](https://www.alura.com.br/)
+
+### Para o projeto foi utilizado:
+
+- Java
+- Springboot
+- Typescript
+- React
+
+Com este projeto eu pude consolidar meus conhecimentos em uso/criação de apis e desenvolvimento de interfaces gráficas.
--- src/main/java/br/com/alura/screenmatch/config/CorsConfiguration.java
+++ src/main/java/br/com/alura/screenmatch/config/CorsConfiguration.java
@@ -11,6 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
